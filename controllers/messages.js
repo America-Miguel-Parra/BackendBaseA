b6ed3d32eafc32143e5c35bd445e9ef5c9208d28
America Miguel Parra <isc19350307@gmail.com>
2022-09-27 22:17:02 -0500

    first commit

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,26 +1,26 @@
 const { request, response } = require("express")
 
 const rootMessage =(req=request, res=response) =>{
-    res.json({msg:"Mensajes"})
+    res.status(404).json({msg:"Mensajes"})
 }
 
 const hiMessages =(req=request, res=response)=>{
-    res.json({msg:"Hola mundo"})  
+    res.status(403).json({msg:"Hola mundo"})  
 }
 
 const byeMessages =(req=request, res=response)=>{
-    res.json({msg:"Adios mundo"})  
+    res.status(408).json({msg:"Adios mundo"})  
 }
 
 const postMessage =(req=request, res=response)=>{
-    res.json({msg:"Mensaje POST"})  
+    res.status(401).json({msg:"Mensaje POST"})  
 }
 
 const putMessage =(req=request, res=response)=>{
-    res.json({msg:"Mensaje put"})  
+    res.status(400).json({msg:"Mensaje put"})  
 }
 
 const deleteMessage =(req=request, res=response)=>{
-    res.json({msg:"Mensaje delete"})  
+    res.status(405).json({msg:"Mensaje delete"})  
 }
 module.exports ={rootMessage,hiMessages,byeMessages, postMessage, putMessage, deleteMessage}
