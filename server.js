b884e61bbf62073ec00f02f3551287192dcbf127
America Miguel Parra <isc19350307@gmail.com>
2022-10-13 10:38:28 -0500

    usuarios

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const messagesRouter = require ('./routes/messages')
+const usuariosRouter = require ('./routes/usuarios')
 const cors = require("cors")
 
 class Server {
     constructor(){
         this.app = express()
         this.paths = {
-            messages: "/api/v1/messages"
+            messages: "/api/v1/messages",
+            usuarios: "/api/v1/usuarios"
         }
         
         this.middlewares()
@@ -15,6 +17,7 @@ class Server {
     }
 routes(){
     this.app.use(this.paths.messages, messagesRouter)
+    this.app.use(this.paths.usuarios, usuariosRouter)
     }
 
     middlewares(){
